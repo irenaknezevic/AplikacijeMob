e5b5f1da60c10a37f03cfd3f5aa41796d5380d90
student <irena123456@vmti.hr>
2019-11-15 18:48:27 +0100

    15.11. lv2

--- app/src/main/java/com/example/mojaaplikacija/MainActivity.java
+++ app/src/main/java/com/example/mojaaplikacija/MainActivity.java
@@ -7,14 +7,16 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button oBtnDalje;
-    private String sImePrezime;
+    private String sIme;
+    private String sPrezime;
+    private String sDatum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +27,23 @@ public class MainActivity extends AppCompatActivity {
         oBtnDalje.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View y) {
-                TextInputEditText oInputImePrezime = (TextInputEditText)findViewById(R.id.imePrezime);
+                TextInputEditText oInputIme = (TextInputEditText)findViewById(R.id.ime);
+                TextInputEditText oInputPrezime = (TextInputEditText)findViewById(R.id.prezime);
+                EditText oEditTextDatum = (EditText)findViewById(R.id.etUnos);
 
-                sImePrezime = oInputImePrezime.getText().toString();
-                if(sImePrezime.matches("")) {
+                sIme = oInputIme.getText().toString();
+                sPrezime = oInputPrezime.getText().toString();
+                sDatum = oEditTextDatum.getText().toString();
+
+
+                if(sIme.matches("") && sPrezime.matches("") && sDatum.matches("")) {
 
                 }
                 else {
                     Intent in = new Intent(MainActivity.this, StudentInfoActivity.class);
-                    in.putExtra("imePrezime", sImePrezime);
+                    in.putExtra("ime", sIme);
+                    in.putExtra("prezime", sPrezime);
+                    in.putExtra("datum", sDatum);
                     startActivity(in);
                 }
             }
--- app/src/main/java/com/example/mojaaplikacija/StudentInfoActivity.java
+++ app/src/main/java/com/example/mojaaplikacija/StudentInfoActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -15,6 +15,10 @@ public class StudentInfoActivity extends AppCompatActivity {
 
     private Button oBtnSljedeca;
     private String sPredmet;
+    private String sProf;
+    private String sAkGod;
+    private String sPredavanja;
+    private String sLv;
     private String sZapis;
 
     @Override
@@ -27,19 +31,36 @@ public class StudentInfoActivity extends AppCompatActivity {
             @Override
             public void onClick(View y) {
                 TextInputEditText oInputPredmet = (TextInputEditText)findViewById(R.id.predmet);
+                EditText oEditTextProf =(EditText)findViewById(R.id.profIme);
+                EditText oEditTextAkGodina =(EditText)findViewById(R.id.editAkGod);
+                EditText oEditTextBrPredavanja =(EditText)findViewById(R.id.etSatiPredavanja);
+                EditText oEditTextBrLv =(EditText)findViewById(R.id.etSatiLv);
 
                 sPredmet = oInputPredmet.getText().toString();
-                if(sPredmet.matches("")) {
+                sProf = oEditTextProf.getText().toString();
+                sAkGod = oEditTextAkGodina.getText().toString();
+                sPredavanja = oEditTextBrPredavanja.getText().toString();
+                sLv = oEditTextBrLv.getText().toString();
+
+                if(sPredmet.matches("") && sProf.matches("") && sAkGod.matches("") && sPredavanja.matches("") && sLv.matches("")) {
 
                 }
                 else {
                     Intent i = getIntent();
-                    String mojeIme = i.getExtras().getString("imePrezime");
+                    String mojeIme = i.getExtras().getString("ime");
+                    String mojePrezime = i.getExtras().getString("prezime");
+                    String datum = i.getExtras().getString("datum");
 
-                    sZapis = mojeIme + " upisali ste " + sPredmet;
+                    sZapis = mojeIme + " " + mojePrezime + " (" + datum +") ";
 
                     Intent inn = new Intent(StudentInfoActivity.this, SummaryActivity.class);
                     inn.putExtra("zapisUnosa", sZapis);
+                    inn.putExtra("predmet", sPredmet);
+                    inn.putExtra("profesor", sProf);
+                    inn.putExtra("akGod", sAkGod);
+                    inn.putExtra("predavanja", sPredavanja);
+                    inn.putExtra("lv", sLv);
+
                     startActivity(inn);
                 }
             }
